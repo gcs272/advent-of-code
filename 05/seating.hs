b60068802a0b99e7c8d125f798c89bc688f40d9e
Graham Smith <graham@adhocteam.us>
2020-12-06 20:27:57 -0500

    day 5, part two

--- 05/seating.hs
+++ 05/seating.hs
@@ -14,5 +14,16 @@ seat :: String -> Integer
 seat (s) = let ((a, _), (b, _)) = (sbin(0, take 7 s), sbin(0, drop 7 s)) in a * 8 + b
 
 main = do
-    contents <- getContents
-    print (maximum (map seat (lines contents)))
+  contents <- getContents
+
+  -- day one
+  print (maximum (map seat (lines contents)))
+
+  -- part two
+  print (let (all, seats) = ([1..127*8+7], map seat (lines contents)) in
+    head (filter (\sid ->
+      (elem (sid - 1) seats) &&
+      (not (elem sid seats)) &&
+      (elem (sid + 1) seats)
+      ) all
+    ))
