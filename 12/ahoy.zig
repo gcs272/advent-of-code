92d7f6f23f66fb57f0d870564389604309607a76
Graham Smith <graham@adhocteam.us>
2020-12-12 22:45:31 -0500

    day 12, part two

--- 12/ahoy.zig
+++ 12/ahoy.zig
@@ -17,11 +17,13 @@ fn clamp(heading: i32) i32 {
     return heading;
 }
 
-pub fn main() !void {
+pub fn readLines() ![]u8 {
     var ainst = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = &ainst.allocator;
-    const input = try std.io.getStdIn().readToEndAlloc(alloc, 1024 * 1024);
+    return std.io.getStdIn().readToEndAlloc(alloc, 1024 * 1024);
+}
 
+fn partOne(input: []u8) !i32 {
     var heading: i32 = 0;
     var posx: i32 = 0;
     var posy: i32 = 0;
@@ -62,5 +64,68 @@ pub fn main() !void {
 
         //std.debug.print("{}\t{d},{d}\t{d}\n", .{ line, posx, posy, heading });
     }
-    std.debug.print("part one: {}\n", .{abs(posx) + abs(posy)});
+
+    return abs(posx) + abs(posy);
+}
+
+fn partTwo(input: []u8) !i32 {
+    var wayx: i32 = 10;
+    var wayy: i32 = 1;
+    var shipx: i32 = 0;
+    var shipy: i32 = 0;
+
+    var lines = std.mem.tokenize(input, "\n");
+    while (lines.next()) |line| {
+        var inst = line[0];
+        var val = try std.fmt.parseInt(i32, line[1..], 10);
+
+        if (inst == 'R' or inst == 'L') {
+            if (inst == 'L') {
+                val = 360 - val;
+            }
+
+            var t = wayx;
+            if (val == 90) {
+                wayx = wayy;
+                wayy = -t;
+            } else if (val == 180) {
+                wayx = -wayx;
+                wayy = -wayy;
+            } else if (val == 270) {
+                wayx = -wayy;
+                wayy = t;
+            }
+        }
+
+        wayx += switch (inst) {
+            'E' => val,
+            'W' => -val,
+            else => 0,
+        };
+
+        wayy += switch (inst) {
+            'N' => val,
+            'S' => -val,
+            else => 0,
+        };
+
+        if (inst == 'F') {
+            shipx += wayx * val;
+            shipy += wayy * val;
+        }
+
+        //std.debug.print("{}\t{d},{d}\t{d},{d}\n", .{ line, shipx, shipy, wayx, wayy });
+    }
+
+    return abs(shipx) + abs(shipy);
+}
+
+pub fn main() !void {
+    var lines = try readLines();
+
+    const a1 = try partOne(lines);
+    std.debug.print("part one: {d}\n", .{a1});
+
+    const a2 = try partTwo(lines);
+    std.debug.print("part two: {d}\n", .{a2});
 }
