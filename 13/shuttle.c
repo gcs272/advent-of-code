0e46948d94bf9e11404aa8887a7418d7f5db4e7e
Graham Smith <graham@adhocteam.us>
2020-12-14 16:29:55 -0500

    day 13, part two

--- 13/shuttle.c
+++ 13/shuttle.c
@@ -3,8 +3,9 @@
 #include <limits.h>
 
 #define MAXLINE 1024
+#define MAXBUSSES 1024
 
-int main() {
+int one() {
   int minute;
   char line[MAXLINE];
   char *token;
@@ -34,6 +35,54 @@ int main() {
     }
   }
 
-  printf("bus %d arrives in %d minutes, answer=%d", bus, wait, bus * wait);
-  return 0;
+  return bus * wait;
+}
+
+long int two() {
+  char line[MAXLINE];
+  char *token;
+  int delay = 0;
+  long int timestamp = 0;
+  long int stride = 1;
+
+  scanf("%*d%s", line);
+
+  token = strtok(line, ",");
+  while(1) {
+    // skip the 'x' records'
+    if (*token != 'x') {
+      long int cbus;
+      sscanf(token, "%ld", &cbus);
+      printf("\nreading record: %d\t%ld\t(timestamp=%ld, stride=%ld)", delay, cbus, timestamp, stride);
+      fflush(stdout);
+
+      // find the first instance and stride for the
+      // accumulated pair and the current bus
+      long int next_ts = -1;
+      while(1) {
+        timestamp = timestamp + stride;
+        if (timestamp % cbus == (delay % cbus)) {
+          if (next_ts == -1) {
+            next_ts = timestamp;
+          } else {
+            stride = timestamp - next_ts;
+            timestamp = next_ts;
+            break;
+          }
+        }
+      }
+    }
+
+    // get the next token in line
+    token = strtok(NULL, ",");
+    if (token == NULL) {
+      return stride - timestamp;
+    }
+    delay++;
+  }
+}
+
+int main() {
+  //printf("\npart one=%d", one());
+  printf("\npart two=%ld", two());
 }
