887ea8e6a1b9523fc00d0ae271fb4f3d801ca0fe
Graham Smith <graham@adhocteam.us>
2020-12-29 14:40:52 -0500

    day 24, part two

--- 24/hex.go
+++ 24/hex.go
@@ -27,6 +27,38 @@ func walk(path string, sx, sy int) (int, int) {
 	return sx, sy
 }
 
+func life(black map[[2]int]bool) map[[2]int]bool {
+	// grab all counts of all neighbors
+	neighbors := map[[2]int]int{}
+	for pos, _ := range black {
+		for _, d := range [][2]int{{-2, 0}, {2, 0}, {1, -1}, {1, 1}, {-1, -1}, {-1, 1}} {
+			neighbors[[2]int{pos[0] + d[0], pos[1] + d[1]}]++
+		}
+	}
+
+	next := map[[2]int]bool{}
+	for pos, count := range neighbors {
+		if count == 2 {
+			next[pos] = true
+		}
+	}
+
+	// flipped tiles stay if they have 1 or 2 neighbors
+	for pos, _ := range black {
+		filled := 0
+		for _, d := range [][2]int{{-2, 0}, {2, 0}, {1, -1}, {1, 1}, {-1, -1}, {-1, 1}} {
+			if _, ok := black[[2]int{pos[0] + d[0], pos[1] + d[1]}]; ok {
+				filled++
+			}
+		}
+		if filled == 1 || filled == 2 {
+			next[pos] = true
+		}
+	}
+
+	return next
+}
+
 func main() {
 	contents, _ := ioutil.ReadAll(os.Stdin)
 	black := map[[2]int]bool{}
@@ -39,5 +71,11 @@ func main() {
 		}
 	}
 
-	fmt.Printf("%#v", len(black))
+	fmt.Printf("one=%d", len(black))
+
+	for i := 0; i < 100; i++ {
+		black = life(black)
+	}
+
+	fmt.Printf("\ntwo=%d", len(black))
 }
