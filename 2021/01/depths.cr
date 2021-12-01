73a2ab264943bd56ebb1c9870877bdd6ca5831b6
Graham Smith <graham@adhocteam.us>
2021-12-01 13:57:12 -0500

    s/map/reduce

--- 2021/01/depths.cr
+++ 2021/01/depths.cr
@@ -1,4 +1,4 @@
 vals = File.read_lines("input").map { |l| l.to_i64 }
-dayone = (1..vals.size()-1).map { |i| vals[i] > vals[i-1] ? 1 : 0 }.sum
-daytwo = (3..vals.size()-1).map { |i| vals[i] > vals[i-3] ? 1 : 0}.sum
+dayone = (1..vals.size()-1).reduce(0) { |acc, i| vals[i] > vals[i-1] ? acc + 1 : acc }
+daytwo = (3..vals.size()-1).reduce(0) { |acc, i| vals[i] > vals[i-3] ? acc + 1 : acc }
 puts "one=#{dayone} two=#{daytwo}"
